9d6d8c4160e8515945436bf81cb8616438de0aff
Jessica Maccaro <jmacc003@pelican.ib.hpcc.ucr.edu>
2020-11-06 09:08:00 -0800

    Ready to grade

--- sevenless.py
+++ sevenless.py
@@ -4,3 +4,9 @@ start = 0
 end   = 99
 divisor=7
 print("Printing out numbers from",start,"to",end, " not divisible by",divisor)
+
+nums= []
+for x in range(0,100):
+        if (x%7 is not 0):
+                nums.append(str(x))
+print('\n'.join(nums))
